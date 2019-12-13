39eefebabd8895e5ac348c3e066321b50ae14fec
Itisafeature <thebigdebugger@gmail.com>
2019-12-13 00:12:18 -0500

    added up to initialize new game function

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -8,3 +8,71 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.dice').style.display = 'none'
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    // 1 Random Number
+    dice = Math.floor(Math.random() * 6) + 1;
+
+    // 2 Display the Result
+    var diceDom = document.querySelector('.dice');
+    diceDom.style.display = 'block';
+    diceDom.src = `dice-${dice}.png`;
+
+
+    // 3 Update the round score
+    if (dice !== 1) {
+        roundScore += dice; 
+        document.querySelector(`#current-${activePlayer}`).textContent = roundScore; 
+    } else {
+        nextPlayer();
+    }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    // Add current score to global score
+    scores[activePlayer] += roundScore
+        
+    // Update the UI
+    document.querySelector(`#score-${activePlayer}`).textContent = scores[activePlayer]
+    // Check if the player won the game 
+
+    if (scores[activePlayer] >= 20) {
+        document.getElementById(`name-${activePlayer}`).textContent = "Winner!";
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner');
+        document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active');
+    } else {
+        nextPlayer();
+    }
+
+});
+
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
+    
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+
+
+}
+
+
+
